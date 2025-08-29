7333430599b53a42e4d76b8f2f80268d7f50a1b8
Tim <tim.daubner@outlook.com>
2025-08-29 14:09:00 +0200

    fetch 1Gen pokemon func and show them

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
-function DOTHIS() {
-    console.log("Hello World");
+
+// async function getPokemnonInfo() {
+//     getBadPromise().then((result) => {
+//         console.log(result);
+//     }).catch((error) => {
+//         console.error(error);
+//     })
+// }
+
+async function getPokemonInfo(start, end) {
+    try {
+        let response = (await fetch(`${fetchBaseDataPokemonAPI()}/pokemon?offset=${start}&limit=${end}`));
+        let pokemonGen = await response.json();
+        getPokemonProperties(pokemonGen);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getPokemonProperties(pokemonGen) {
+    try {
+        let storage = [];
+        for (let i = 0; i < pokemonGen.results.length; i++) {
+            let data = pokemonGen.results[i].url;
+            responsePokemonData = await fetch(data);
+            responsePokemonData = await responsePokemonData.json();
+            storage[i] = responsePokemonData;
+        }
+        await loadPokemon(storage);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function fetchBaseDataPokemonAPI() {
+    let baseURL = "https://pokeapi.co/api/v2";
+    return baseURL;
+}
+
+function loadPokemon(data) {
+    document.getElementById('content').innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        document.getElementById('content').innerHTML += getHTMLForPokeCard(data, i);
+    }
+}
+
+function getHTMLForPokeCard(data, i) {
+    return `
+    <div id="${data[i].id}" class="poke-card">
+    <img class="sprite" src="${data[i].sprites.front_default}">
+    <div class="data">
+        <p>Name: ${data[i].name}</p>
+        <p>Height: ${data[i].height}</p>
+        <p>Weight: ${data[i].weight}</p>
+    </div>
+    </div>`
+}
+
+// async function fetchPath(url) {
+//     let response = await fetch(url.toString());
+//     return await response.json();
+// }
+
+function myFetch() {
+    console.log(responseData);
 }
